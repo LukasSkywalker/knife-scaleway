d41937a93c23b9b45701c461f6ab23dbd1d16064
Roland Moriz <roland@moriz.de>
2013-01-31 14:39:00 +0100

    refactoring

--- lib/chef/knife/digital_ocean_droplet_list.rb
+++ lib/chef/knife/digital_ocean_droplet_list.rb
@@ -1,8 +1,3 @@
-#
-# Author:: Adam Jacob (<adam@opscode.com>)
-# Copyright:: Copyright (c) 2009 Opscode, Inc.
-# License:: Apache License, Version 2.0
-#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -15,74 +10,52 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-require 'digital_ocean'
+require File.expand_path('../digital_ocean_base', __FILE__)
 require 'highline'
-require 'chef/knife'
-require 'chef/json_compat'
-require 'tempfile'
 
 class Chef
   class Knife
     class DigitalOceanDropletList < Knife
+      include Knife::DigitalOceanBase
 
       banner 'knife digital_ocean droplet list (options)'
 
-      option :digital_ocean_client_id,
-        :short       => '-K CLIENT_ID',
-        :long        => '--digital_ocean_client_id CLIENT_ID',
-        :description => 'Your DigitalOcean client_id',
-        :proc        => Proc.new { |client_id| Chef::Config[:knife][:digital_ocean_client_id] = client_id }
-
-      option :digital_ocean_api_key,
-        :short       => '-A API_KEY',
-        :long        => '--digital_ocean_api_key API_KEY',
-        :description => 'Your DigitalOcean API_KEY',
-        :proc        => Proc.new { |api_key| Chef::Config[:knife][:digital_ocean_api_key] = api_key }
-
-      def h
-        @highline ||= HighLine.new
-      end
-
       def run
-        unless Chef::Config[:knife][:digital_ocean_client_id] && Chef::Config[:knife][:digital_ocean_api_key]
-          puts Chef::Config[:knife].inspect
-          ui.error("Missing Credentials")
-          exit 1
-        end
-
         $stdout.sync = true
 
+        validate!
+
         droplet_list = [
-            h.color('ID', :bold),
-            h.color('Name', :bold),
+            h.color('ID',     :bold),
+            h.color('Name',   :bold),
+            h.color('Size',   :bold),
             h.color('Region', :bold),
-            h.color('IP', :bold),
-            h.color('Image', :bold),
+            h.color('IPv4',   :bold),
+            h.color('Image',  :bold),
             h.color('Status', :bold)
         ]
 
-        regions = {}
-        client.regions.list.regions.each do |region|
-          regions[region.id] = region.name
+        regions = client.regions.list.regions.inject({}) do |h, region|
+          h[region.id] = region.name
+          h
+        end
+
+        sizes = client.sizes.list.sizes.inject({}) do |h, size|
+          h[size.id] = size.name
+          h
         end
 
         client.droplets.list.droplets.each do |droplet|
           droplet_list << droplet.id.to_s
           droplet_list << droplet.name.to_s
-          droplet_list << droplet.ip_address.to_s
+          droplet_list << sizes[droplet.size_id]
           droplet_list << regions[droplet.region_id]
+          droplet_list << droplet.ip_address.to_s
           droplet_list << droplet.image_id.to_s + ' (' + client.images.show(droplet.image_id).image.name +  ')'
           droplet_list << droplet.status.to_s
         end
 
-        puts h.list(droplet_list, :columns_across, 6)
-      end
-
-
-      def client
-        DigitalOcean::API.new(:client_id => Chef::Config[:knife][:digital_ocean_client_id],
-                              :api_key   => Chef::Config[:knife][:digital_ocean_api_key])
+        puts h.list(droplet_list, :uneven_columns_across, 7)
       end
 
     end
