bc8d9e038fa336374b376b69e26d953e0058b51c
Roland Moriz <roland@moriz.de>
2013-01-31 16:04:55 +0100

    add droplet destroy

---
+++ lib/chef/knife/digital_ocean_droplet_destroy.rb
@@ -0,0 +1,43 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require File.expand_path('../digital_ocean_base', __FILE__)
+
+class Chef
+  class Knife
+    class DigitalOceanDropletDestroy < Knife
+      include Knife::DigitalOceanBase
+
+      banner 'knife digital_ocean droplet destroy (options)'
+
+      option :server,
+        :short       => '-S ID',
+        :long        => '--server ID',
+        :description => 'The server id'
+
+      def run
+        $stdout.sync = true
+
+        validate!
+
+        unless locate_config_value(:server)
+          ui.error("Server cannot be empty. ALL DATA WILL BE LOST! => -S <server-id>")
+          exit 1
+        end
+
+        result = client.droplets.delete locate_config_value(:server)
+        puts result.status
+      end
+
+    end
+  end
+end
