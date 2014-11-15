bef4f3a095099d9d60e6d296876a5d7f1d7d5eee
Greg Fitzgerald <greg@gregf.org>
2014-11-14 21:14:45 -0500

    Add knife digital_ocean image destroy -I <id>

---
+++ lib/chef/knife/digital_ocean_image_destroy.rb
@@ -0,0 +1,42 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+require 'chef/knife/digital_ocean_base'
+
+class Chef
+  class Knife
+    class DigitalOceanImageDestroy < Knife
+      include Knife::DigitalOceanBase
+
+      banner 'knife digital_ocean image destroy (options)'
+
+      option :id,
+        :short       => '-I ID',
+        :long        => '--image-id ID',
+        :description => 'The image ID'
+
+      def run
+        $stdout.sync = true
+
+        validate!
+
+        unless locate_config_value(:id)
+          ui.error("Image ID cannot be empty. => -I <image-id>")
+          exit 1
+        end
+
+        result = client.images.delete(id: locate_config_value(:id))
+        ui.error JSON.parse(result)['message'] rescue 'OK'
+      end
+    end
+  end
+end
